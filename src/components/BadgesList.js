fee48a6d5c14f452638fefd5baa7ab2854247810
jucetina <juliethcetina@gmail.com>
2021-02-28 23:50:39 -0500

    Se crea un custom hook para filtrar los badges de acuerdo a un criteriode búsqueda ingresado en un campo que también se agregó

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 
 import './styles/BadgesList.css';
 
+function useSearchBadges(badges){
+    const [ query, setQuery ] = useState('');
+    const [ filteredBadges, setFilteredBadges] = useState(badges);
+
+    useMemo(() => {
+            const result = badges.filter(badge => {
+                return `${badge.firstName} ${badge.lastName}`.toLowerCase().includes(query.toLowerCase());
+            });
+            setFilteredBadges(result);
+    }
+    , [ badges, query ]);
+
+    return { query, setQuery, filteredBadges };
+}
+
 export default function BadgesList({ badges }){
-    if(badges.length === 0){
+
+    const { query, setQuery, filteredBadges } = useSearchBadges(badges);
+
+    const handleChange = e => {
+        setQuery(e.target.value);
+    }
+
+    if(filteredBadges.length === 0){
         return( 
             <>
+                <div className="form-group">
+                    <label>FilterBadges</label>
+                    <input type="text" className="form-control" value={query} onChange={handleChange} />
+                </div> 
                 <h3>No badges were found</h3>
                 <Link to="badges/new" className="btn btn-primary">
                     Create the first Badge!
@@ -16,25 +42,31 @@ export default function BadgesList({ badges }){
     }
 
     return(
-        <ul>
-            {badges.map((badge) => {
-                return(
-                    <div className="BadgeItem" key={badge.id}>
-                        <div>
-                            <img className="BadgeItem__avatar" src={badge.avatarUrl} alt={`${badge.firstName} ${badge.lastName} Avatar`}/>
+        <>
+            <div className="form-group">
+                <label>FilterBadges</label>
+                <input type="text" className="form-control" value={query} onChange={handleChange} />
+            </div> 
+            <ul className="list-unstyled">
+                {filteredBadges.map((badge) => {
+                    return(
+                        <div className="BadgeItem" key={badge.id}>
+                            <div>
+                                <img className="BadgeItem__avatar" src={badge.avatarUrl} alt={`${badge.firstName} ${badge.lastName} Avatar`}/>
+                            </div>
+                            <div>
+                                <li className="BadgeItem__detail">
+                                    <Link className="text-reset text-decoration-none" to={`/badges/${badge.id}`}>
+                                        <p><strong>{badge.firstName} {badge.lastName}</strong></p>
+                                        <p className="BadgeItem__detail-twitter"><span className="BadgeItem__twitterIcon"></span>@{badge.twitter}</p>
+                                        <p>{badge.jobTitle}</p>
+                                    </Link>
+                                </li>
+                            </div>
                         </div>
-                        <div>
-                            <li className="BadgeItem__detail">
-                                <Link className="text-reset text-decoration-none" to={`/badges/${badge.id}`}>
-                                    <p><strong>{badge.firstName} {badge.lastName}</strong></p>
-                                    <p className="BadgeItem__detail-twitter"><span className="BadgeItem__twitterIcon"></span>@{badge.twitter}</p>
-                                    <p>{badge.jobTitle}</p>
-                                </Link>
-                            </li>
-                        </div>
-                    </div>
-                )
-            })}
-        </ul>
+                    )
+                })}
+            </ul>
+        </>
     )
 }
