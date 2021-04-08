6d9891c4b0b82134ea7835eebed0b7bc59fa4dff
jucetina <juliethcetina@gmail.com>
2021-04-08 02:01:42 -0500

    Se cambia a className en componente NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,7 +5,7 @@ import './styles/NotFound.css';
 export default function NotFound(){
     return(
         <>
-            <div class="NotFound">
+            <div className="NotFound">
                 <h1 className="NotFound__title">Error 404: Not Found!</h1>
             </div>
         </>
